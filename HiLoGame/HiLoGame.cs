7f72b7133b32802c22f5d89631d842a4fcdce7bd
Bartek <bartlomiej.kacki@gmail.com>
2024-06-02 21:25:10 +0200

    Add project files.

---
+++ HiLoGame/HiLoGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiLoGame
+{
+    internal static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, (MAXIMUM+1));
+        private static int nextNumber;
+        private static int pot = 10;
+        public static int GetPot() { return pot; }
+
+        public static void Guess(bool higherOrLower)
+        {
+            nextNumber = random.Next(1, (MAXIMUM + 1));
+
+            if (higherOrLower && (nextNumber >= currentNumber))
+            {
+                Console.WriteLine("Zgadłeś!");
+                pot++;
+            }
+            else if (!higherOrLower && (nextNumber <= currentNumber))
+            {
+                Console.WriteLine("Zgadłeś!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Niestety błędna odpowiedź.");
+                pot--;
+            }
+            currentNumber = nextNumber;
+            Console.WriteLine($"Aktualna wartość to: {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int halfOfMaximum = MAXIMUM / 2;
+
+            if (currentNumber >= halfOfMaximum)
+                Console.WriteLine($"Liczba wynosi co najmniej {halfOfMaximum}");
+            else
+                Console.WriteLine($"Liczba jest mniejsza niż {halfOfMaximum}");
+
+            pot--;
+        }
+    }
+}
---
+++ HiLoGame/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HiLoGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w HiLo!");
+            Console.WriteLine($"Odgadnij liczbę od 1 do {HiLoGame.MAXIMUM}.");
+            HiLoGame.Hint();
+            while(HiLoGame.GetPot() > 0)
+            {
+                Console.WriteLine("Wciśnij [w]iększa, [m]niejsza lub [?] kupno wskazówki");
+                Console.WriteLine($"Inne klawisze to koniec gry z {HiLoGame.GetPot()}zł.");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (keyChar == 'w')
+                    HiLoGame.Guess(true);
+                else if (keyChar == 'm')
+                    HiLoGame.Guess(false);
+                else if (keyChar == '?')
+                    HiLoGame.Hint();
+                else
+                    return;
+            }
+            Console.WriteLine("Skończyły Ci się pieniądze. Do zobaczenia!");
+        }
+    }
+}
